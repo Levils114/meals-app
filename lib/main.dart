ee2821deda545a17ab85c648a45b751158de4205
Levils114 <levils114@gmail.com>
2021-05-25 19:34:25 -0300

    feat: navigate with routes

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:meals_app/screens/categories_meals_screen.dart';
 import 'package:meals_app/screens/categories_screen.dart';
+import 'package:meals_app/utils/app_routes.dart';
 
 void main() => runApp(MyApp());
 
@@ -11,17 +13,19 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.pink,
         accentColor: Colors.amber,
-        fontFamily: 'Raleway',
         canvasColor: Color.fromRGBO(255, 254, 229, 1),
         textTheme: ThemeData.light().textTheme.copyWith(
               headline6: TextStyle(
                 fontSize: 20,
-                fontFamily: 'RobotoCondensed',
+                fontFamily: 'Raleway',
               ),
             ),
       ),
-      home: CategoriesScreen(),
       debugShowCheckedModeBanner: false,
+      routes: {
+        AppRoutes.HOME: (_) => CategoriesScreen(),
+        AppRoutes.CATEGORIES_MEALS: (_) => CategoriesMealsScreen(),
+      },
     );
   }
 }
